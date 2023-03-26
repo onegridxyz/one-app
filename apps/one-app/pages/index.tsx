e1e5c5fbdf6a596db06a518eb3a51dc0d021b912
Truong Le <travistrle@gmail.com>
2023-03-26 06:07:08 -0500

    OG-31 clean up NX

--- apps/one-app/pages/index.tsx
+++ apps/one-app/pages/index.tsx
@@ -1,18 +1,7 @@
 import styles from './index.module.scss';
 
 export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
-  return (
-    <div className={styles.page}>
-      <div className="wrapper">
-        <div className="container">OneGrid.xyz</div>
-      </div>
-    </div>
-  );
+  return <div className={styles.page}>OneGrid.xyz</div>;
 }
 
 export default Index;
