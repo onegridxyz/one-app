98a70641d045d72f17106a133a0a7b2e0872f39b
Truong Le <travistrle@gmail.com>
2023-04-10 17:54:47 -0500

    OG-32 add login constant

--- apps/one-app/api/authenticate/Login.ts
+++ apps/one-app/api/authenticate/Login.ts
@@ -20,3 +20,13 @@ export const postAuthenticate = (loginRequest: LoginRequest) => {
     password: loginRequest.password
   });
 }
+
+// create an async function to call the API by using axios with the loginRequest
+export const login = async (loginRequest: LoginRequest) => {
+  const baseUrl = environment.gatewayUrl
+  const requestUrl = `${baseUrl}/api/authenticate`;
+    const { username, password } = loginRequest;
+    return axios.post<LoginResponse>(requestUrl, {
+      username, password
+    }).then((data) => { return data });
+}
---
+++ apps/one-app/constants/LocalStorageConstants.ts
@@ -0,0 +1,3 @@
+export class LocalStorageConstants {
+  public static readonly ACCESS_TOKEN = 'id_token';
+}
